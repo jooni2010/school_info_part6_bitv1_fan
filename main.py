77a0274ee6a64b3010ad5c611e57d9fda415b73e
jooni2010 <141928403+jooni2010@users.noreply.github.com>
2023-12-14 10:33:07 +0900

    pxt.json, main.blocks, main.ts, main.py 업데이트하기

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_button_pressed_a():
+    pins.digital_write_pin(DigitalPin.P0, 1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    pins.digital_write_pin(DigitalPin.P0, 0)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    pins.digitalWritePin(DigitalPin.P0, 0)
 })
